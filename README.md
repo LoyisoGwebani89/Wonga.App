3740e159fd51eee0ca897ec87360d4055cbc820d
LoyisoGwebani89 <38100608+LoyisoGwebani89@users.noreply.github.com>
2018-08-16 15:56:09 +0200

    Add files via upload
    Fixed empty string check

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
 # Wonga.App
-This is an app for sending messages between 2 .Net Core Console Applications using RabbitMQ.
-
-
-## Prerequisites to run the source code
-- Visual Studio 2017 15.8.0
-- .Net Core 2.1.
-- Install Erlang 21.0.1 here  http://www.erlang.org/downloads, this is a prerequisite to install  RabbitMQ on Windows
-- Install RabbitMQ 3.7.7 for Windows here https://www.rabbitmq.com/install-windows.html
-- Refer to https://www.rabbitmq.com/install-windows.html for prerequisites of running RabbitMQ on other Operating Systems 
-
-## Instructions for using the app
-1. Set both WongaReceiver.App and WongaSender.App as startup projects on VS
-2. Run the multiple startup projects
-3. Two console windows will be opened. Put the side by side for visibility
-4. Type a name on the SENDER WONGA APP console window and see the results on both console windos
-
-
- 
+An app for sending messages between 2 .Net Core console applications using RabbitMQ
--- WongaSenderApp/Program.cs
+++ WongaSenderApp/Program.cs
@@ -19,14 +19,24 @@ namespace WongaSender.App
             var serviceProvider = GetServiceProvider();
             var messageService = serviceProvider.GetService<IMessageService>();
 
+            var name = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Please enter your name");
+                name = Console.ReadLine();
+            }
+
             //send message
             var messageModel = new MessageModel
             {
-                Name = Console.ReadLine(),
+                Name = name,
                 Type = MessageType.Message,
                 MessageHandler = ShowMessage
             };
+
             messageService.Send(messageModel);
+
 
             //subscribe to message response
             var responseModel = new MessageModel
@@ -45,7 +55,7 @@ namespace WongaSender.App
                 .AddSingleton<IAppSettings, AppSettings>()
                 .AddTransient<IMessageService, MessageService>()
                 .AddTransient<ILoggerFactory>(x => new LoggerFactory())
-                .AddTransient< IErrorLogger, ErrorLogger <Program>>()
+                .AddTransient<IErrorLogger, ErrorLogger<Program>>()
                 .BuildServiceProvider();
         }
 
